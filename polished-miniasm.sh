ebc0cdba90bd9efd6175ef288377db97fe54ec2f
Felix Bemm <felix.bemm@tuebingen.mpg.de>
2017-03-31 07:24:16 +0200

    Removed export cmds and added comments.

--- polished-miniasm.sh
+++ polished-miniasm.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export SF=/ebio/abt6_projects9/abt6_software/bin
-export PATH=$SF/minimap:$PATH
-export PATH=$SF/racon/bin:$PATH
-export PATH=$SF/pilon:$PATH
-
 SEQ=$1
 PE1=$2
 PE2=$3
@@ -18,32 +13,38 @@ IT2=$BSE"_IT2"
 IT3=$BSE"_IT3"
 IT4=$BSE"_IT4"
 
-minimap -Sw5 -L100 -m0 -t$CPU $$EQ $SEQ > $IT0".paf"
+# Assembled Long Reads
 
+minimap -Sw5 -L100 -m0 -t$CPU $$EQ $SEQ > $IT0".paf"
 miniasm -f $SEQ $IT0".paf" > $ASM".gfa"
+
+# Extract Sequences from GFA
 
 awk '$1 ~/S/ {print ">"$2"\n"$3}' $ASM > $IT0".fasta"
 
-minimap -t$CPU $IT0".fasta" $SEQ > $IT1".paf"
+# Run 1st Racon Iteration
 
+minimap -t$CPU $IT0".fasta" $SEQ > $IT1".paf"
 racon $SEQ $IT1".paf" $IT0".fasta" $IT1".fasta"  
 
-minimap -t$CPU $IT1".fasta" $SEQ > $IT2".paf"
+# Run 2nd Racon Iteration
 
+minimap -t$CPU $IT1".fasta" $SEQ > $IT2".paf"
 racon $SEQ $IT2".paf" $IT1".fasta" $IT2".fasta"
 
-minimap -t$CPU $IT2".fasta" $SEQ > $IT3".paf"
+# Run 3rd Racon Iteration
 
+minimap -t$CPU $IT2".fasta" $SEQ > $IT3".paf"
 racon $SEQ $IT3".paf" $IT2".fasta" $IT3".fasta"
 
-minimap -t$CPU $IT3".fasta" $SEQ > $IT4".paf"
+# Run 4th Racon Iteration
 
+minimap -t$CPU $IT3".fasta" $SEQ > $IT4".paf"
 racon $SEQ $IT4".paf" $IT3".fasta" $IT4".fasta"
 
+# Run final Pilon polishing
+
 bwa index $IT4".fasta"
-
 bwa mem -t $CPU $IT4".fasta" $PE1 $PE2 | samtools view -bS | samtools sort -@ $CPU > $IT4".bam"
-
 samtools index $IT4".bam"
-
 java -Xmx200G -jar pilon-1.21.jar --genome $IT4".fasta" --frags $IT4".bam" --output $IT5".fasta" --threads $CPU
